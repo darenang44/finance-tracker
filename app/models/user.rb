45c4b884b98879bcf211d142c097ec91e483d6c8
darenang44 <darenang44@gmail.com>
2016-12-26 13:55:59 -0800

    Add view to listing stocks and delete stock

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,46 +7,25 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
 
-  # # creating restrictions to see if we can add the stock
-  # def can_add_stock?(ticker_symbol)
-  #   # if the under_stock_limit? and !stock_already_added?(ticker_symbol) returns true then we can add stock
-  #   under_stock_limit? && !stock_already_added?(ticker_symbol)
-  # end
-  #
-  # # restriction to where you can only track 10 stock at a time
-  # def under_stock_limit?
-  #   (user_stocks.count < 10)
-  # end
-  #
-  # # restriction where you can't track a stock twice
-  # def stock_already_added?(ticker_symbol)
-  #   # we are finding the stock by using the find_by_ticker method
-  #   stock = Stock.find_by_ticker(ticker_symbol)
-  #   # we are returning false unless the stock is found
-  #   return false unless stock
-  #   # so if the stock does exist then the statement below will return true
-  #   user_stocks.where(stock_id: stock.id).exists?
-  # end
-
+  # creating restrictions to see if we can add the stock
   def can_add_stock?(ticker_symbol)
-
+    # if the under_stock_limit? and !stock_already_added?(ticker_symbol) returns true then we can add stock
     under_stock_limit? && !stock_already_added?(ticker_symbol)
-
   end
 
+  # restriction to where you can only track 10 stock at a time
   def under_stock_limit?
-
     (user_stocks.count < 10)
-
   end
 
+  # restriction where you can't track a stock twice
   def stock_already_added?(ticker_symbol)
-
+    # we are finding the stock by using the find_by_ticker method
     stock = Stock.find_by_ticker(ticker_symbol)
-
+    # we are returning false unless the stock is found
     return false unless stock
-
+    # so if the stock does exist then the statement below will return true
     user_stocks.where(stock_id: stock.id).exists?
-
   end
+
 end
