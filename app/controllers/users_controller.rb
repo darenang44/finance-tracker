5c2dbf433313958039e4007b1985704cec2d2413
darenang44 <darenang44@gmail.com>
2016-12-26 20:40:41 -0800

    Add ajax for friends search and add search and add_friend action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,31 @@ class UsersController < ApplicationController
 
   def my_friends
     @friendships = current_user.friends
-
   end
+
+  def search
+    # we are going to find the user/friend by the search_params which is the input given in our search bar
+    @users = User.search(params[:search_params])
+
+    if @users
+      # we want all the users to show up expect our selves (current_user)
+      @users = current_user.expect_current_user(@users)
+      render partial: 'friends/lookup'
+    else
+      # if user is not found then nothing
+      render status: :not_found, nothing: true
+    end
+  end
+
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+
+    if current_user.save
+      redirect_to my_friends_path, notice: "Friend was successfully added."
+    else
+      redirect_to my_friends_path, flash[:error] = "There was an error with adding friend"
+    end
+  end
+
 end
